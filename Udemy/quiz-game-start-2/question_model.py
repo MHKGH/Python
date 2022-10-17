bd50717c7aca3383616b016d32e68ecce74ac04a
MHKGH <hihdinfo@gmail.com>
2022-10-17 16:32:39 +0530

    adding udemy

---
+++ Udemy/quiz-game-start-2/question_model.py
@@ -0,0 +1,13 @@
+from data import question_data
+from quiz_brain import QuizBrain
+
+class Question:
+    def __init__(self,q_text,q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+
+
+
+
+
